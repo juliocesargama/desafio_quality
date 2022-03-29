88f9374deaf22ef4a72250c34f99fddfda5618b9
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-29 15:21:13 -0300

    [fix] refatorando o codigo sem a validacao

--- src/main/java/br/com/meli/desafio_quality/entity/RealEstate.java
+++ src/main/java/br/com/meli/desafio_quality/entity/RealEstate.java
@@ -4,17 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RealEstate {
-  
-  @NotBlank(message = "O campo propName não pode ser vazio")
-  @Pattern(regexp = "[A-Z][a-záàâãéèêíïóôõöúçñ]+", message = "O campo propName deve começar com uma letra maiúscula.")
-  @Size(min = 3, max = 30, message = "O campo propName deve ter entre 3 e 30 caracteres")
+
   private String propName;
 }
