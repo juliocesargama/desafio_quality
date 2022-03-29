ec477c995da9e0ca6858d58c9d6bb214612ba4b6
Julio César Gama <juliocesar.gama@mercadolivre.com>
2022-03-29 16:43:23 -0300

    RealEstateController.java created

--- src/main/java/br/com/meli/desafio_quality/controller/RealEstateController.java
+++ src/main/java/br/com/meli/desafio_quality/controller/RealEstateController.java
@@ -1,14 +1,36 @@
 package br.com.meli.desafio_quality.controller;
 
+import br.com.meli.desafio_quality.entity.RealEstate;
+import br.com.meli.desafio_quality.service.RealEstateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RealEstateController {
 
-    @GetMapping("/realestate")
-    public void getRealEstates(){
+    @Autowired
+    RealEstateService realEstateService;
 
+    @GetMapping("/realestate/all")
+    public ResponseEntity<List<RealEstate>> getAllRealEstates(){
+        return ResponseEntity.ok(realEstateService.getAll());
     }
 
+    @GetMapping("/realestate/{name}")
+    public ResponseEntity<RealEstate> getRealEstateByName(@PathVariable String propName){
+        return ResponseEntity.ok(realEstateService.getRealEstate(propName));
+    }
+
+    @GetMapping("/realestate/{propName}/totalarea")
+    public ResponseEntity<Double> getRealEstateTotalArea(@PathVariable String propName){
+
+        RealEstate realEstate = realEstateService.getRealEstate(propName);
+
+        return ResponseEntity.ok(realEstateService.getRealStateTotalArea(realEstate));
+    }
 }
