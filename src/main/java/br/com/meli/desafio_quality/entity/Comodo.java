e0b70e647268518e8aaf9b10157621555bce46b7
Julio César Gama <juliocesar.gama@mercadolivre.com>
2022-03-29 15:04:44 -0300

    Created Comodo Entity

---
+++ src/main/java/br/com/meli/desafio_quality/entity/Comodo.java
@@ -0,0 +1,16 @@
+package br.com.meli.desafio_quality.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comodo {
+
+    private roomName String;
+    private roomWidth Double;
+    private roomLength Double;
+
+}
