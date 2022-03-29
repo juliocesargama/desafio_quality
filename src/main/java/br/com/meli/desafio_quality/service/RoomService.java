6f379bc2e71519965f407d6fee9201f1f250445f
Hugo Freire <ahugo.freire@gmail.com>
2022-03-29 16:59:56 -0300

    add method getBiggestRoom

--- src/main/java/br/com/meli/desafio_quality/service/RoomService.java
+++ src/main/java/br/com/meli/desafio_quality/service/RoomService.java
@@ -1,10 +1,14 @@
 package br.com.meli.desafio_quality.service;
 
+import br.com.meli.desafio_quality.entity.RealEstate;
 import br.com.meli.desafio_quality.entity.Room;
 import org.springframework.stereotype.Service;
 
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class RoomService {
@@ -22,5 +26,10 @@ public class RoomService {
                 .get();
     }
 
+    public Room getBiggestRoom(RealEstate realEstate) {
+        List<Room> rooms = realEstate.getRooms();
+        rooms.sort(Comparator.comparing(room -> getRoomArea(room).shortValue()));
 
+        return rooms.get(rooms.size() - 1);
+    }
 }
