87e436d404f393b2f1cc8965c7467e18dcd3903e
Marcelo <marcelo.leite@mercadolivre.com>
2022-03-30 16:25:03 -0300

    [feat] created test to getAll() method

--- src/test/java/br/com/meli/desafio_quality/service/RealEstateServiceTest.java
+++ src/test/java/br/com/meli/desafio_quality/service/RealEstateServiceTest.java
@@ -4,6 +4,7 @@ import br.com.meli.desafio_quality.entity.District;
 import br.com.meli.desafio_quality.entity.RealEstate;
 import br.com.meli.desafio_quality.entity.Room;
 import br.com.meli.desafio_quality.entity.RoomAreaDTO;
+import br.com.meli.desafio_quality.repository.RealEstateRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +22,9 @@ public class RealEstateServiceTest {
 
     @Mock
     private RoomService roomService;
+
+    @Mock
+    private RealEstateRepository realEstateRepository;
 
     @InjectMocks
     private RealEstateService realEstateService;
@@ -48,5 +52,37 @@ public class RealEstateServiceTest {
         Assertions.assertEquals(dto1.getRoomArea(), areaList.get(0).getRoomArea());
         Assertions.assertEquals(dto2.getRoomArea(), areaList.get(1).getRoomArea());
 
+    }
+
+    /**
+     * @author Marcelo Leite
+     */
+
+    @Test
+    public void getAllRealEstateTest() {
+        Room r1 = new Room("TestRoom", 10.0, 15.0);
+        Room r2 = new Room("TestRoom2", 30.0, 30.0);
+        List<Room> roomList = new ArrayList<>();
+        roomList.add(r1);
+        roomList.add(r2);
+
+        List<RealEstate> realEstateList = new ArrayList<>();
+        RealEstate realEstate = new RealEstate("Casa", new District("Melicidade", BigDecimal.valueOf(500)), roomList);
+        realEstateList.add(realEstate);
+
+        Mockito.when(realEstateRepository.findAll()).thenReturn(realEstateList);
+
+        Assertions.assertEquals(realEstateList, realEstateService.getAll());
+
+    }
+
+    @Test
+    public void getAllRealEstateEmptyTest() {
+        //Caso de retornar uma lista vazia
+        List<RealEstate> realEstateList = new ArrayList<>();
+
+        Mockito.when(realEstateRepository.findAll()).thenReturn(realEstateList);
+
+        Assertions.assertEquals(realEstateList.isEmpty(), realEstateService.getAll());
     }
 }
